7a02368c60dddee821cb6748fa4022b1c3657bb9
Devesh patni <patnid@uni.coventry.ac.uk>
2016-12-02 23:38:21 +0000

    Delete 3_highest_perfect_square_pseudocode.py

--- Week2/3_highest_perfect_square_pseudocode.py
+++
@@ -1,8 +0,0 @@
-PERFECT_SQUARE(number)
-    n <- integer(number)
-    for x <- 1 to n
-        if n < x ** 2
-            n <- (x-1)**2 // get to the lower number then x
-            return n
-    elif n = x ** 2
-        return n
