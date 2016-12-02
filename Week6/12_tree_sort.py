a043b0579a264493782a1ff950dc4bf804816176
Devesh patni <patnid@uni.coventry.ac.uk>
2016-12-02 23:33:28 +0000

    Add files via upload

---
+++ Week6/12_tree_sort.py
@@ -0,0 +1,60 @@
+class BinTreeNode(object):
+ 
+    def __init__(self, value):
+        self.value=value
+        self.left=None
+        self.right=None
+       
+def tree_insert( tree, item):
+    if tree==None:
+        tree=BinTreeNode(item)
+    else:
+        if(item < tree.value):
+            if(tree.left==None):
+                tree.left=BinTreeNode(item)
+            else:
+                tree_insert(tree.left,item)
+        else:
+            if(tree.right==None):
+                tree.right=BinTreeNode(item)
+            else:
+                tree_insert(tree.right,item)
+    return tree
+ 
+def postorder(tree):
+    if(tree.left!=None):
+        postorder(tree.left)
+    if(tree.right!=None):
+        postorder(tree.right)
+    print (tree.value)
+ 
+ 
+def in_order(tree):
+
+    stack = []
+    done = 0
+
+    while not done:  
+
+        if tree != None:        
+            stack.append(tree)  #aappend the root node to the stack
+            tree = tree.left    
+        else:
+            if (len(stack) > 0):
+                tree = stack.pop()#pop the most recent value from stack
+                print(tree.value)
+                tree = tree.right#pointer goes to the right value
+            else:
+                done = 1
+ 
+if __name__ == '__main__':
+   
+  t=tree_insert(None,6);
+  tree_insert(t,10)
+  tree_insert(t,5)
+  tree_insert(t,2)
+  tree_insert(t,3)
+  tree_insert(t,4)
+  tree_insert(t,11)
+  in_order(t)
+
